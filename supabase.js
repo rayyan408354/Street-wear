fd8ba781c93ef6775274c764ff046d0c3c835c46
rayyan408354 <rayyanbirth2007@gmail.com>
2026-08-16 14:40:25 +0530

    Fix Supabase client connection

--- supabase.js
+++ supabase.js
@@ -1,2 +1,9 @@
 const SUPABASE_URL = "https://ezfldwapvcownpozhxww.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_NOoVPD0PlqvsdEhiFh6Spg_ctXTd1ve";
+
+const SUPABASE_PUBLISHABLE_KEY =
+  "sb_publishable_NOoVPD0PlqvsdEhiFh6Spg_ctXTd1ve";
+
+const supabaseClient = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_PUBLISHABLE_KEY
+);
